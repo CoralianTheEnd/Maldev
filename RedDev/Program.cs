fa086a22a77b8aa3ab1e39524daec168472e3061
natha <natha@NATHANDESKTOP>
2024-04-27 21:13:38 -0700

    Add project files.

---
+++ RedDev/Program.cs
@@ -0,0 +1,36 @@
+﻿using RedDev.Shellcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedDev
+{
+    internal class Program
+    {
+        // # Managed delegate for our shell code function
+        private delegate IntPtr Delegate_PEBShellCode();
+
+        // # Our shellcode to execute
+        private static byte[] Shellcode_ReadFSWord30 = new byte[]
+        {
+            0x64, 0xA1, 0x30, 0x00, 0x00, 0x00,         // mov eax, dword ptr fs:[30]
+            0xC3                                        // ret
+        };
+
+        static void Main(string[] args)
+        {
+            // # Cast our newly stage shellcode to a managed delegate
+            Delegate_PEBShellCode GetPebAddress =
+                (Delegate_PEBShellCode)SharpStager.StageShellcode<Delegate_PEBShellCode>(Shellcode_ReadFSWord30);
+
+            // # Execute our new function
+            IntPtr pebAddress = GetPebAddress();
+
+            Console.WriteLine("PEB is located at: 0x{0:X8}", pebAddress);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RedDev/Shellcode/SharpStager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
+
+
+/*
+ * Based off At0ms example .Net 4.0
+ */
+
+namespace RedDev.Shellcode
+{
+    public class SharpStager
+    {
+
+        public static object StageShellcode<TDelegate>(byte[] shellcode)
+        {
+            // # Unfortunetly we're not allowed to change memory permission after creating, looking into possible ways
+            using (MemoryMappedFile mmf = MemoryMappedFile.CreateNew(null, shellcode.Length, MemoryMappedFileAccess.ReadWriteExecute))
+            {
+                // # Both MemoryMappedViewViewAccessor & MemoryMappedViewStream give us access needed
+                using (MemoryMappedViewAccessor mmva = mmf.CreateViewAccessor(0, shellcode.Length, MemoryMappedFileAccess.ReadWriteExecute))
+                {
+                    unsafe
+                    {
+                        fixed (byte* pShellcode = shellcode)
+                        {
+                            byte* pFunc = null;
+
+                            // # Need to use AcquirePoiner to get a valid pointer to the memory mapped, unsafe handle returns incorrectly.
+                            mmva.SafeMemoryMappedViewHandle.AcquirePointer(ref pFunc);
+
+                            if (pFunc == null) return null;
+
+                            // # Copy over shellcode to memory mapped file & zero original array.
+                            CopyMemory(pFunc, pShellcode, shellcode.Length);
+
+                            ZeroMemory(pShellcode, shellcode.Length);
+
+                            // # Convert unmanaged function pointer to managed callable function pointer.
+                            // # No public access further into GetDelegateForFunctionPointer without having to entirely recreate it.
+                            // # Can't direct cast (void*|byte*) of function to delegate in 4.0
+                            return (TDelegate)(object)Marshal.GetDelegateForFunctionPointer((IntPtr)pFunc, typeof(TDelegate));
+
+                        }
+                    }
+
+                }
+            }
+        }
+  
+        public unsafe static void CopyMemory(byte* dest, byte* src, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                dest[i] = src[i];
+            }
+        }
+
+        public unsafe static void ZeroMemory(byte* dest, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                dest[i] = 0;
+            }
+        }
+    }
+}
